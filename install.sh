ac10ab002276e17d18126e4a20f64003e6d475a9
Rony Lee <rongyil33@gmail.com>
2022-06-06 11:21:26 +0800

    Update alacritty background color, install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,4 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 
 cp $SCRIPT_DIR/.vimrc ~
-cp $SCRIPT_DIR/.tmux.conf ~
 cp -r $SCRIPT_DIR/.config ~
