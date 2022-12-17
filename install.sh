3b0f7e4cdbb37c1956807c8e9f00f3060da849c1
Rony Lee <rongyil33@gmail.com>
2022-12-17 11:07:19 +0800

    Add TPM Installation

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+TPM=$HOME/.tmux/plugins/tpm
 
 cp -r $SCRIPT_DIR/.config ~
 cp -r $SCRIPT_DIR/.local ~
-mkdir -p ~/.tmux
+if [ ! -d "$TPM" ]; then
+    mkdir -p ~/.tmux
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 cp $SCRIPT_DIR/.tmux/iceberg_minimal.tmux.conf ~/.tmux/iceberg_minimal.tmux.conf
