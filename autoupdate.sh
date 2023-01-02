8d718c47dce20de7dbbab2d8b6666b2f2b1e50d3
Rony Lee <rongyil33@gmail.com>
2023-01-02 18:22:17 +0800

    Add qutebrowser

--- autoupdate.sh
+++ autoupdate.sh
@@ -7,4 +7,8 @@ rm -r ./.config/nvim && cp -r ~/.config/nvim $SCRIPT_DIR/.config
 cp -r ~/.config/zathura $SCRIPT_DIR/.config
 cp -r ~/.config/tmux/tmux.conf $SCRIPT_DIR/.config/tmux
 cp -r ~/.config/fish/functions/* $SCRIPT_DIR/.config/fish/functions
-cp -r ~/.config/qutebrowser $SCRIPT_DIR/.config
+cp ~/.config/qutebrowser/autoconfig.yml $SCRIPT_DIR/.config/qutebrowser
+cp -r ~/.config/qutebrowser/bookmarks $SCRIPT_DIR/.config/qutebrowser
+cp ~/.config/qutebrowser/config.py $SCRIPT_DIR/.config/qutebrowser
+cp -r ~/.config/qutebrowser/greasemonkey $SCRIPT_DIR/.config/qutebrowser
+cp ~/.config/qutebrowser/quickmarks $SCRIPT_DIR/.config/qutebrowser
