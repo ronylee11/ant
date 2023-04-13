02ef9fe3f090f6a973d68c47a69ca0441185dcf9
Rony Lee <rongyil33@gmail.com>
2023-04-13 14:34:08 +0800

    Add vimrc to init.vim

--- autoupdate.sh
+++ autoupdate.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && 
 
 cp ~/.config/nvim/.vimrc $SCRIPT_DIR/.config/nvim/.vimrc
 cp -r ~/.config/alacritty $SCRIPT_DIR/.config
-rm -r ./.config/nvim && cp -r ~/.config/nvim $SCRIPT_DIR/.config
+rm -r $SCRIPT_DIR/.config/nvim && cp -r ~/.config/nvim $SCRIPT_DIR/.config
 cp -r ~/.config/zathura $SCRIPT_DIR/.config
 cp -r ~/.config/tmux/tmux.conf $SCRIPT_DIR/.config/tmux
 cp -r ~/.config/fish/functions/* $SCRIPT_DIR/.config/fish/functions
